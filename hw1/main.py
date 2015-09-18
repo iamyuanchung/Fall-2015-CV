9c7a46ca391ade6c43d2e37e63196af6219c3b61
andyyuan <iamyuanchung@gmail.com>
2015-09-19 00:25:32 +0800

    task 1, 2, 3, 4 and 6 done

---
+++ hw1/main.py
@@ -0,0 +1,78 @@
+import sys
+
+import numpy as np
+import cv2
+
+
+def upside_down(img):
+    img_ans = np.zeros(img.shape, np.int)
+    for i in xrange(img.shape[0]):
+        img_ans[i, :] = img[img.shape[0] - i - 1, :]
+    return img_ans
+
+def right_side_left(img):   # TODO
+    img_ans = np.zeros(img.shape, np.int)
+    for j in xrange(img.shape[1]):
+        img_ans[:, j] = img[:, img.shape[1] - j - 1]
+    return img_ans
+
+def diag_mirror(img):   # TODO
+    return right_side_left(upside_down(img))
+
+def rotate(img):
+    img_ans = np.zeros((img.shape[1], img.shape[0]), np.int)
+    for i in xrange(img.shape[0]):
+        for j in xrange(img.shape[1]):
+            img_ans[j][img.shape[0] - i - 1] = img[i][j]
+    return img_ans
+
+def threshold(img, basic):
+    for i in xrange(img.shape[0]):
+        for j in xrange(img.shape[1]):
+            if img[i][j] > 128:
+                img[i][j] = 255
+            else:
+                img[i][j] = 0
+    return img
+
+def main():
+    # usage: python ./main.py [task_id]
+    # task_id = 1 ~ 6
+    assert len(sys.argv) == 2
+    assert int(sys.argv[1]) > 0 \
+       and int(sys.argv[1]) < 7
+
+    img = cv2.imread('lena.bmp', 0)
+    # img is a 512 x 512 np.array
+
+    if sys.argv[1] == '1':
+        #TODO: upside-down
+        img_ans = upside_down(img)
+        cv2.imwrite('upside_down.bmp', img_ans)
+
+    elif sys.argv[1] == '2':
+        # TODO: right-side-left
+        img_ans = right_side_left(img)
+        cv2.imwrite('right_side_left.bmp', img_ans)
+
+    elif sys.argv[1] == '3':
+        # TODO: diagonally mirrored
+        img_ans = diag_mirror(img)
+        cv2.imwrite('diag_mirror.bmp', img_ans)
+
+    elif sys.argv[1] == '4':
+        # TODO: rotate lena 45 degrees clockwise
+        img_ans = rotate(img)
+        cv2.imwrite('rotate.bmp', img_ans)
+
+    # elif sys.argv[1] == '5':
+        # TODO: shrink lena in half
+
+    else:
+        # TODO: binarize lena at 128 to get a binary image
+        img_ans = threshold(img, 128)
+        cv2.imwrite('threshold_128.bmp', img)
+
+
+if __name__ == '__main__':
+    main()
