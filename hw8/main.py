d0f3439f606d10950add7e73f1060e94afafa1af
andyyuan <iamyuanchung@gmail.com>
2015-11-24 22:47:12 +0800

    add functions for task 1 and 2

--- hw8/main.py
+++ hw8/main.py
@@ -4,6 +4,21 @@ import numpy as np
 import cv2
 
 
+def generate_Gaussian_noise(img, mu, sigma, amplitude):
+    return img + amplitude * np.random.normal(mu, sigma, img.shape)
+
+def generate_salt_and_pepper_noise(img, low, high, threshold):
+    prob_map = np.random.uniform(low, high, img.shape)
+    img_sp = copy.deepcopy(img)
+    for i in xrange(img.shape[0]):
+        for j in xrange(img.shape[1]):
+            if prob_map[i][j] < threshold:
+                img_sp[i][j] = 0
+            elif prob_map[i][j] > 1 - threshold:
+                img_sp[i][j] = 255
+    return img_sp
+
+
 def main():
     img = cv2.imread('lena.bmp', 0)
     # img is now a 512 x 512 numpy.ndarray
@@ -11,43 +26,25 @@ def main():
     # Generate and output an image with
     # additive white Gaussian noise
     # with amplitude = 10
-    img_gauss_10 = img + 10 * np.random.normal(0, 1, img.shape)
+    img_gauss_10 = generate_Gaussian_noise(img, 0, 1, 10)
     cv2.imwrite('lena.gaussian.10.bmp', img_gauss_10)
 
     # Generate and output an image with
     # additive white Gaussian noise
     # with amplitude = 30
-    img_gauss_30 = img + 30 * np.random.normal(0, 1, img.shape)
+    img_gauss_30 = generate_Gaussian_noise(img, 0, 1, 30)
     cv2.imwrite('lena.gaussian.30.bmp', img_gauss_30)
 
     # Generate and output an image with
     # salt-and-pepper noise with
     # threshold = 0.05
-    prob_map = np.random.uniform(0, 1, img.shape)
-    img_sp_5 = np.zeros(img.shape, np.int)
-    for i in xrange(img.shape[0]):
-        for j in xrange(img.shape[1]):
-            if prob_map[i][j] < 0.05:
-                img_sp_5[i][j] = 0
-            elif prob_map[i][j] > 1 - 0.05:
-                img_sp_5[i][j] = 255
-            else:
-                img_sp_5[i][j] = img[i][j]
-    cv2.imwrite('lena.sp.05.bmp', img_sp_5)
+    img_sp_05 = generate_salt_and_pepper_noise(img, 0, 1, 0.05)
+    cv2.imwrite('lena.sp.05.bmp', img_sp_05)
 
     # Generate and output an image with
     # salt-and-pepper noise with
     # threshold = 0.1
-    prob_map = np.random.uniform(0, 1, img.shape)
-    img_sp_10 = np.zeros(img.shape, np.int)
-    for i in xrange(img.shape[0]):
-        for j in xrange(img.shape[1]):
-            if prob_map[i][j] < 0.1:
-                img_sp_10[i][j] = 0
-            elif prob_map[i][j] > 1 - 0.1:
-                img_sp_10[i][j] = 255
-            else:
-                img_sp_10[i][j] = img[i][j]
+    img_sp_10 = generate_salt_and_pepper_noise(img, 0, 1, 0.1)
     cv2.imwrite('lena.sp.10.bmp', img_sp_10)
 
     
